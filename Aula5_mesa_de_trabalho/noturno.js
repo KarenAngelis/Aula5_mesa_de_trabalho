8229f82b1065ce35e991b8510e35c69789c108c3
KarenAngelis <karen.denich@gmail.com>
2023-03-06 21:30:09 -0300

    mai um detalhe

--- Aula5_mesa_de_trabalho/noturno.js
+++ Aula5_mesa_de_trabalho/noturno.js
@@ -1,7 +1,7 @@
 
 window.onload = function(){
   document.body.style.background = "rgb(39, 39, 39)";
-  document.querySelectorAll("h1").forEach(function(h1){
+  document.querySelectorAll(".h1").forEach(function(h1){
     h1.style.color = "rgb(170, 170, 170)";
     h1.style.backgroundColor = "rgb(19, 19, 19)";
     h1.style.borderColor = "black";
