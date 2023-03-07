21a8b09f92068e4ec02dacd83f6dc207b182172a
KarenAngelis <karen.denich@gmail.com>
2023-03-06 21:29:03 -0300

    melhorando o código

--- Aula5_mesa_de_trabalho/noturno.js
+++ Aula5_mesa_de_trabalho/noturno.js
@@ -1,9 +1,12 @@
 
 window.onload = function(){
   document.body.style.background = "rgb(39, 39, 39)";
-  document.getElementsByTagName("h1")[0].style.color = "rgb(170, 170, 170)";
-  document.getElementsByTagName("h1")[0].style.backgroundColor = "rgb(19, 19, 19)"
-  document.getElementsByTagName("h1")[0].style.borderColor = "black"
+  document.querySelectorAll("h1").forEach(function(h1){
+    h1.style.color = "rgb(170, 170, 170)";
+    h1.style.backgroundColor = "rgb(19, 19, 19)";
+    h1.style.borderColor = "black";
+  });
+
   document.querySelectorAll(".item").forEach(function(item){
     item.style.backgroundColor = "rgb(19, 19, 19)";
     item.style.border = "black";
